05d5f9fc82abd64dd3995753596aa65c41e489b8
msultanafz <msultanalfaritz20@gmail.com>
2024-12-04 04:14:21 +0700

    sseelleesaaii

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./public/**/*.html"],
   theme: {
     extend: {
-      backgroundImage: {
-        "my-background": "url('/img/medical-background.jpg')",
-      },
       fontFamily: {
         lora: ["Lora"],
       },
